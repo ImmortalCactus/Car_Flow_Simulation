6e031a97e9c4eb60ef68d26b712f11af36f7bcd8
ImmortalCactus <ImmortalCactus@github.com>
2021-12-31 21:10:47 +0800

    .

--- simulation.py
+++ simulation.py
@@ -1,25 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
+import random
 import time
-import heapq
+import copy
 
+ALPHA = 0.1
 class Graph:
-    def __init__(self, vertices, edges):
+    def __init__(self, vertices, edges, time_interval):
+        self.timer = 0
         self.vertices = vertices
         self.edges = edges
-        self.edge_lengths = [np.linalg.norm(self.vertices[e[0]]-self.vertices[e[1]]) for e in self.edges]
+        self.edge_lengths = np.array([np.linalg.norm(self.vertices[e[0]]-self.vertices[e[1]]) for e in self.edges])
+        self.edge_cost = self.edge_lengths.copy()
         self.cars = []
+        self.time_interval = time_interval
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111)
+        self.ax.set_aspect('equal', adjustable='box')
     def plot(self):
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.set_aspect('equal', adjustable='box')
-        x, y = self.vertices.T
-        x1 = [self.vertices[i[0]][0] for i in self.edges]
-        x2 = [self.vertices[i[1]][0] for i in self.edges]
-        y1 = [self.vertices[i[0]][1] for i in self.edges]
-        y2 = [self.vertices[i[1]][1] for i in self.edges]
-        plt.plot(x1, y1, x2, y2, marker = 'o')
+        for e in self.edges:
+            plt.plot([self.vertices[e[0]][0], self.vertices[e[1]][0]],[self.vertices[e[0]][1], self.vertices[e[1]][1]], color="blue")
+        ani = animation.FuncAnimation(self.fig, self.update, frames = 1200, interval=self.time_interval, blit = True)
         plt.show()
     def shortest_path(self, s, t):
         distance = np.ones((self.vertices.shape[0])) * np.inf
@@ -29,21 +32,39 @@ class Graph:
         while(done == 0):
             done = 1
             for i, e in enumerate(self.edges):
-                if(distance[e[0]]+self.edge_lengths[i] < distance[e[1]]):
-                    distance[e[1]] = distance[e[0]]+self.edge_lengths[i]
+                if(distance[e[0]]+self.edge_cost[i] < distance[e[1]]):
+                    distance[e[1]] = distance[e[0]]+self.edge_cost[i]
                     src[e[1]] = i
                     done = 0
-        if distance[t] == np.inf: return []
+                # assume bidirectional edge
+        if distance[t] == np.inf:
+            return []
         path = [t]
         while(path[0]!=s):
             path = [self.edges[src[path[0]]][0], src[path[0]]] + path
         return path
     
-    def add_car(self, s, t):
-        self.cars.append(self.Car(s, t, self.shortest_path(s, t), self))
+    def add_car(self, s, t, start_time):
+        self.cars.append(self.Car(s, t, start_time, self.shortest_path(s, t), self))
 
+    def update(self, i):
+        new_cost = self.edge_lengths.copy()
+        for c in self.cars:
+            if not c.on_vertex:
+                new_cost[c.loc_edge]+=self.edge_lengths[c.loc_edge]
+        self.edge_cost = (1-ALPHA)*self.edge_cost + (ALPHA)*new_cost
+        l = []
+        for c in self.cars:
+            if self.timer >= c.start_time:
+                c.update()
+                coord = c.coordinates()
+                c.dot.set_data(coord[0],coord[1])
+                l.append(c.dot)
+        self.timer += self.time_interval
+        return l
     class Car:
-        def __init__(self, init_loc, destination, designated_path, graph):
+        def __init__(self, init_loc, destination, start_time, designated_path, graph):
+            self.start_time = start_time
             self.on_vertex = True
             self.loc_vertex = init_loc
             self.loc_edge = -1
@@ -52,21 +73,22 @@ class Graph:
             self.destination = destination
             self.speed = float(1)
             self.graph = graph
-        def update(self, interval_length):
-            time_left = interval_length
+            self.dot, = self.graph.ax.plot([],[],marker="o", linestyle='', markersize=5, color = "red")
+        def update(self):
+            time_left = self.graph.time_interval
             while(time_left > 0):
                 if(self.on_vertex):
                     if(self.loc_vertex == self.destination):
                         return 1
                     self.adjust_path() 
-                    self.adjust_speed()
                     self.designated_path = self.designated_path[1:]
                     self.on_vertex = False
+                    if(len(self.designated_path) == 0):
+                        return 1
                     self.loc_edge = self.designated_path[0]
-                    self.pos_edge = self.speed*time_left/self.graph.edge_lengths[self.loc_edge]
-                else:
-                    self.pos_edge += self.speed*time_left/self.graph.edge_lengths[self.loc_edge]
-
+                    
+                self.adjust_speed()
+                self.pos_edge += self.speed*time_left/self.graph.edge_lengths[self.loc_edge]
                 if(self.pos_edge >= 1):
                     time_left = (self.pos_edge - 1)*self.graph.edge_lengths[self.loc_edge]/self.speed
                     self.loc_vertex = self.graph.edges[self.loc_edge][1]
@@ -79,33 +101,42 @@ class Graph:
             return 0
 
         def adjust_path(self):
+            self.designated_path = self.graph.shortest_path(self.designated_path[0],self.designated_path[-1])
             return
         def adjust_speed(self):
+            count = 0
+            for c in self.graph.cars:
+                if(c.loc_edge == self.loc_edge and c.pos_edge > self.pos_edge):
+                    count = count + 1
+            self.speed = 1/(count+1)
             return
-        def coordinaties(self):
+        def coordinates(self):
             if(self.on_vertex):
                 return self.graph.vertices[self.loc_vertex]
             else:
                 return (1-self.pos_edge) * self.graph.vertices[self.graph.edges[self.loc_edge][0]] + self.pos_edge * self.graph.vertices[self.graph.edges[self.loc_edge][1]]
-data = np.array([
-    [1, 1],
-    [1, 2],
-    [1, 3],
-    [2, 1],
-    [2, 2],
-    [2, 3],
-    [3, 1],
-    [3, 2],
-    [3, 3],
-]).astype('float')
-data1 = np.array([
-    [0, 1],
-    [0, 4],
-    [1, 4],
-    [4, 7],
-    [4, 8],
-    [7, 8]
-])
-g = Graph(data, data1)
+
+d = []
+for i in range(10):
+    for j in range(10):
+        d.append([i,j])
+
+data = np.array(d)
+
+d=[]
+for i in range(100):
+    if(i%10 != 9):
+        if(random.uniform(0,1)>0.2):
+            d.append([i,i+1])
+            d.append([i+1,i])
+    if(i<90):
+        if(random.uniform(0,1)>0.2):
+            d.append([i,i+10])
+            d.append([i+10,i])
+data1 = np.array(d)
+g = Graph(data, data1, 0.01)
+
+for i in range(100):
+    g.add_car(0, 99, 0.3*i)
+
 g.plot()
-print(g.shortest_path(0,8))
